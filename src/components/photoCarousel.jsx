c8b6b350b98a68b1aa0b6fedb8f04cd2a80851c0
SHPAKICH <krabic2091@mail.ru>
2024-12-06 12:08:13 +0300

    1.1.1.5

--- src/components/photoCarousel.jsx
+++ src/components/photoCarousel.jsx
@@ -1,53 +1,25 @@
 import React from 'react';
-import { Carousel } from 'react-bootstrap';  // Импорт компонента Carousel из React Bootstrap
+import { Carousel } from 'react-bootstrap'; // Импорт компонента Carousel из React Bootstrap
 
-import catImage from '../media/кошка.jpg';
-import goatImage from '../media/коза.jpg';
-import mouseImage from '../media/мышь.jpg';
-
-const AnimalCarousel = () => {
+const AnimalCarousel = ({ slides }) => {
   return (
     <div className="d-flex justify-content-center">
       <Carousel className="w-50 p-3">
-        <Carousel.Item>
-          <img
-            className="d-block rounded"
-            src={catImage}
-            alt="Кошка"
-            style={{ width: '100%', height: '450px', objectFit: 'cover' }}
-          />
-          <Carousel.Caption>
-            <h5 className="bg-dark" style={{ borderRadius: '50px', padding: '3px' }}>
-              Найден хозяин!
-            </h5>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block rounded"
-            src={goatImage}
-            alt="Коза"
-            style={{ width: '100%', height: '450px', objectFit: 'cover' }}
-          />
-          <Carousel.Caption>
-            <h5 className="bg-dark" style={{ borderRadius: '50px', padding: '3px' }}>
-              Найден хозяин!
-            </h5>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block rounded"
-            src={mouseImage}
-            alt="Мышь"
-            style={{ width: '100%', height: '450px', objectFit: 'cover' }}
-          />
-          <Carousel.Caption>
-            <h5 className="bg-dark" style={{ borderRadius: '50px', padding: '3px' }}>
-              Найден хозяин!
-            </h5>
-          </Carousel.Caption>
-        </Carousel.Item>
+        {slides.map((slide, index) => (
+          <Carousel.Item key={index}>
+            <img
+              className="d-block rounded"
+              src={slide.image}
+              alt={slide.alt}
+              style={{ width: '100%', height: '450px', objectFit: 'cover' }}
+            />
+            <Carousel.Caption>
+              <h5 className="bg-dark" style={{ borderRadius: '50px', padding: '3px' }}>
+                {slide.caption}
+              </h5>
+            </Carousel.Caption>
+          </Carousel.Item>
+        ))}
       </Carousel>
     </div>
   );
--- src/pages/mainPage.jsx
+++ src/pages/mainPage.jsx
@@ -2,11 +2,33 @@ import PhotoCarousel from "../components/photoCarousel";
 import LostAnimalsCard from "../components/lostAnimalscards";
 import SubForm from "../components/subscriptionForm";
 import React from 'react';
+import catImage from '../media/кошка.jpg';
+import goatImage from '../media/коза.jpg';
+import mouseImage from '../media/мышь.jpg';
+
+const slidesData = [
+  {
+    image: catImage,
+    alt: 'Кошка',
+    caption: 'Найден хозяин!',
+  },
+  {
+    image: goatImage,
+    alt: 'Коза',
+    caption: 'Найден хозяин!',
+  },
+  {
+    image: mouseImage,
+    alt: 'Мышь',
+    caption: 'Найден хозяин!',
+  },
+];
+
 
 const MainPage = () => {
     return (
         <div>
-        <PhotoCarousel/>
+        <PhotoCarousel  slides={slidesData}/>
         <LostAnimalsCard/>
         <SubForm/>
         </div>
